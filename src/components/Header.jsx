581e1a338216dbe92ad6ed9315e364bd7778fbc7
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-04-06 14:49:56 -0300

    Primeiro commit, com primeiro requisito feito

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="header">
+        <h1>Sistema Solar</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
